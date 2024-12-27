8a1d5371c95d359fdadf58fa0325c50cf8c137f0
Amit verma <190012191+amitskverma@users.noreply.github.com>
2024-12-27 15:37:36 +0530

    UPDATE

--- obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df02c07aee33cb68956349ca876604cd43e6c6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f907fb01ef321065d6adc73a71c442d2197f9dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
